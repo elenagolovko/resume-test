1c84f1a7cbf725cec30240ed03169cd9ed59aba5
elenagolovko <airdragon220@gmail.com>
2018-09-09 21:02:54 +0300

    base & info

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+    selector: 'my-app',
+    template: `<div class="page-header">It finally fucking works</div>`
+})
+
+export class AppComponent {}
